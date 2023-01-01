d53c34e929777e28032bbb00aa4ce2ad3a54cea7
PenK <spikeb3b@gmail.com>
2023-01-01 15:38:56 -0800

    Naming conventions

--- js/homeListener.js
+++ js/homeListener.js
@@ -2,6 +2,6 @@ document.addEventListener("keydown", pageKeyDown, false);
 
 function pageKeyDown(e) {
     if(e.keyCode==72) {
-        window.location.href = "Index.html";
+        window.location.href = "index.html";
     }
 }  
