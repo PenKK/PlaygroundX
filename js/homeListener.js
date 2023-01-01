41256704a6390df42ce3843ef8dc096c6f27c3ca
PenK <spikeb3b@gmail.com>
2023-01-01 15:24:28 -0800

    Home button fixed

--- js/homeListener.js
+++ js/homeListener.js
@@ -2,6 +2,6 @@ document.addEventListener("keydown", pageKeyDown, false);
 
 function pageKeyDown(e) {
     if(e.keyCode==72) {
-        window.location.href = "project.html";
+        window.location.href = "Index.html";
     }
 }  
