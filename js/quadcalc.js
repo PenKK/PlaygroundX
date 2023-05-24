7500dab7e7cb3a6a3cdf91a43b7dc0bda64f6d06
PenK <spikeb3b@gmail.com>
2023-05-23 19:21:46 -0700

    X-Intercepts and CSS

--- js/quadcalc.js
+++ js/quadcalc.js
@@ -1,50 +1,77 @@
-let aS = 2;
-let bS = 4;
-let cS = 8;
+let a = 1;
+let b = 5;
+let c = 6;
+
+let p = 0;
+let q = 0;
+
+let root = 0;
+let xPlus = 0;
+let xMinus = 0;
 const decimalRounding = 10000;
 
-let inputs = document.querySelectorAll('input');
+ElementId("a").value = a;
+ElementId("b").value = b;
+ElementId("c").value = c;
 
+let inputs = document.querySelectorAll('input');
 inputs.forEach(element => {
     element.addEventListener('input', () => {
         element.style.width = element.value.length + "ch";
 
-        aS = parseInt(ElementId("a").value);
-        bS = parseInt(ElementId("b").value);
-        cS = parseInt(ElementId("c").value);
+        a = parseFloat(ElementId("a").value);
+        b = parseFloat(ElementId("b").value);
+        c = parseFloat(ElementId("c").value);
+
+        if (isNaN(a) || isNaN(b) || isNaN(c) || a == 0) {
+            return;
+        }
 
         calcVertForm();
+        calcX();
     })
 });
 
 calcVertForm = () => {
 
-    let finalVertFormString = "y = ";
+    let formString = "y = ";
 
-    if (isNaN(aS) || isNaN(bS) || isNaN(cS)) {
-        return;
-    }
-    
-    let p = (bS * -1) / (2 * aS);
+    p = (b * -1) / (2 * a);
     // console.log("P: " + p);
-    let q = (aS * Math.pow(p, 2)) + (bS * p) + (cS);
+    q = (a * p**2) + (b * p) + (c);
     // console.log("Q: " + q);
 
-    finalVertFormString = finalVertFormString.concat(aS + "(x ");
+    formString = formString.concat(a + "(x ");
 
     if (p < 0) { 
-        finalVertFormString = finalVertFormString.concat("- " + Math.round(Math.abs(p) * decimalRounding) / decimalRounding  + ")&sup2");
+        formString = formString.concat("+ " + Math.round(Math.abs(p) * decimalRounding) / decimalRounding  + ")&sup2");
     } else {
-        finalVertFormString = finalVertFormString.concat("+ " + Math.round(p * decimalRounding) / decimalRounding + ")&sup2 + ");
+        formString = formString.concat("- " + Math.round(p * decimalRounding) / decimalRounding + ")&sup2");
     }
 
     if (q < 0) {
-        finalVertFormString = finalVertFormString.concat("- " + Math.round(Math.abs(q) * decimalRounding) / decimalRounding);
+        formString = formString.concat(" - " + Math.round(Math.abs(q) * decimalRounding) / decimalRounding);
     } else {
-        finalVertFormString = finalVertFormString.concat(" + " + Math.round(q * decimalRounding) / decimalRounding);
+        formString = formString.concat(" + " + Math.round(q * decimalRounding) / decimalRounding);
     }
 
-    ElementId("vertForm").innerHTML = finalVertFormString;
+    ElementId("vertForm").innerHTML = formString;
+}
+
+calcX = () => {
+    root = (b**2) - (4*a*c);
+
+    if (root < 0) {
+        ElementId("+").innerHTML = "";
+        ElementId("-").innerHTML = "No X-Intercepts";
+    } else {
+        xPlus = (b*-1 + Math.sqrt(root)) / (2*a);
+        xMinus = (b*-1 - Math.sqrt(root)) / (2*a);
+        ElementId("+").innerHTML = "Plus: " + Math.round(xPlus * decimalRounding) / decimalRounding;
+        ElementId("-").innerHTML = "Minus: " + Math.round(xMinus * decimalRounding) / decimalRounding;
+    }
+    
 }
 
 calcVertForm();
+calcX();
