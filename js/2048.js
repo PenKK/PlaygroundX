932b564e628dd0ba6ff580add65af7d62b2814c3
PenK <spikeb3b@gmail.com>
2023-06-14 19:32:45 -0700

    Swiping for mobile

--- js/2048.js
+++ js/2048.js
@@ -397,3 +397,48 @@ storageEvents = () => {
         notification("Achievement complete: 2048 Ambassador");
     }
 }
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+    return evt.touches;
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+        if ( xDiff > 0 ) {
+            left();
+        } else {
+            right();
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            up();
+        } else { 
+            down();
+        }                                                                 
+    }
+
+    xDown = null;
+    yDown = null;                                             
+};
