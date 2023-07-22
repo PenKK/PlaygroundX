51d7edb6d111cebed24d771d438f1b0aafd7d423
PenK <spikeb3b@gmail.com>
2023-07-22 13:05:31 -0700

    Note selection interface complete, started game interface TODO: Audio

--- js/eartrainer.js
+++ js/eartrainer.js
@@ -1,10 +1,27 @@
 const noteSelectionButtonDiv = document.getElementById("noteSelectionButton");
 const noteSelectionMenu = document.getElementById("noteSelection");
+const noteCheckBoxes = document.getElementsByClassName('checkBoxOptions');
+const guessInput = document.getElementById('guessInput');
 
 noteSelectionButtonDiv.onclick = () => {
     if (!noteSelectionMenu.checkVisibility()) {
         noteSelectionMenu.removeAttribute("hidden");
     } else {
         noteSelectionMenu.setAttribute("hidden", "");
+        loadNotes();
+    }
+}
+
+guessInput.addEventListener('input', () => {
+    guessInput.style.width = guessInput.value.length + "ch";
+})
+
+function initializeNotes() {
+    const a = "hi";
+}
+
+function loadNotes() {
+    if (noteCheckBoxes[1].checked === true) {
+        
     }
 }
