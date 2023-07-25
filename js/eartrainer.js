75c800e2fe8c2ed1c34e632acb05c055bf0c46d1
PenK <spikeb3b@gmail.com>
2023-07-24 20:09:26 -0700

    Basically done yippee add piano and add qol: enter key, visual indicators

--- js/eartrainer.js
+++ js/eartrainer.js
@@ -5,19 +5,35 @@ const guessInput = document.getElementById('guessInput');
 const enterButton = document.getElementById('enterButton');
 const nextNoteButton = document.getElementById('nextNote');
 
+let C;
+let D;
+let E;
+let F;
+let G;
+let A;
+let B;
+let CC;
+
+let selectedNotes = [];
+let noteToPlay;
+
 let answer = -1;
+
+initializeNotes();
 
 noteSelectionButtonDiv.onclick = () => {
     if (!noteSelectionMenu.checkVisibility()) {
         noteSelectionMenu.removeAttribute("hidden");
     } else {
         noteSelectionMenu.setAttribute("hidden", "");
-        loadNotes();
+        updateNotes();
     }
 }
 
 nextNoteButton.onclick = () => {
     nextNoteButton.style.visibility = "hidden";
+    generateNote();
+    noteToPlay.play();
 }
 
 enterButton.onclick = () => {
@@ -26,12 +42,49 @@ enterButton.onclick = () => {
     }
 }
 
-function initializeNotes() {
-    const a = "hi";
+listenButton.onclick = () => {
+    noteToPlay.play();
 }
 
-function loadNotes() {
-    if (noteCheckBoxes[1].checked === true) {
-        
+function initializeNotes() {
+    updateNotes();
+}
+
+function updateNotes() {
+    if (noteCheckBoxes[0].checked) { //Trombone  Notes
+        C  = new Audio('https://drive.google.com/uc?export=download&id=1FSiOuIe878CeYK4ahqmPnZpRAmX7rq5_');
+        D  = new Audio('https://drive.google.com/uc?export=download&id=1LYnyYCBiv9twQA6BsX2_u9pwpmI89tDP');
+        E  = new Audio('https://drive.google.com/uc?export=download&id=1JAcT8gNEDJSImk2DJj8w_4nBgEsMzoqW');
+        F  = new Audio('https://drive.google.com/uc?export=download&id=1R9wq0x8gRX9w1MveWAzr5KOMiMPwvReu');
+        G  = new Audio('https://drive.google.com/uc?export=download&id=1RTC1-pAceRv1sFNe3PAZ4Vu0PMHWoaPp');
+        A  = new Audio('https://drive.google.com/uc?export=download&id=1XSanymobasi7z9-Jupdl8Dic7HmywhCI');
+        B  = new Audio('https://drive.google.com/uc?export=download&id=10q9KzPp2jv8_9VqFDG3Z89-ownOH8pRB');
+    } else { //Piano Notes
+        C  = new Audio();
+        D  = new Audio();
+        E  = new Audio();
+        F  = new Audio();
+        G  = new Audio();
+        A  = new Audio();
+        B  = new Audio();
     }
+
+    selectedNotes = [[],[]];
+
+    if (noteCheckBoxes[1].checked) {selectedNotes[0].push(C); selectedNotes[1].push("C")}
+    if (noteCheckBoxes[2].checked) {selectedNotes[0].push(D); selectedNotes[1].push("D")}
+    if (noteCheckBoxes[3].checked) {selectedNotes[0].push(E); selectedNotes[1].push("E")}
+    if (noteCheckBoxes[4].checked) {selectedNotes[0].push(F); selectedNotes[1].push("F")}
+    if (noteCheckBoxes[5].checked) {selectedNotes[0].push(G); selectedNotes[1].push("G")}
+    if (noteCheckBoxes[6].checked) {selectedNotes[0].push(A); selectedNotes[1].push("A")}
+    if (noteCheckBoxes[7].checked) {selectedNotes[0].push(B); selectedNotes[1].push("B")}
+
+    generateNote();
+    
+}
+
+function generateNote() {
+    const noteInt = Math.floor(Math.random()*selectedNotes[0].length);
+    noteToPlay = selectedNotes[0][noteInt];
+    answer = selectedNotes[1][noteInt];
 }
